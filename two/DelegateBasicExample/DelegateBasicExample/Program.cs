c1252726dba9034235d9b110f46cb7a50e329569
danielSajjadi <info.daniel.sajjadi@gmail.com>
2024-03-17 07:32:23 +0100

    make log file

--- two/DelegateBasicExample/DelegateBasicExample/Program.cs
+++ two/DelegateBasicExample/DelegateBasicExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace DelegateBasicExample
 {
@@ -7,7 +8,7 @@ namespace DelegateBasicExample
         delegate void LogDel(string text);
         static void Main(string[] args)
         {
-            LogDel logDel = new LogDel(LogTextToScreen);
+            LogDel logDel = new LogDel(LogTextToFile);
             Console.WriteLine("Please enter your name: ");
             var name = Console.ReadLine();
 
@@ -19,6 +20,14 @@ namespace DelegateBasicExample
         static void LogTextToScreen(string text)
         {
             Console.WriteLine($"{DateTime.Now} : {text}");
+        }
+
+        static void LogTextToFile(string text)
+        {
+            using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.text"), true))
+            {
+                sw.WriteLine($"{DateTime.Now} : {text}");
+            }
         }
     }
 }
