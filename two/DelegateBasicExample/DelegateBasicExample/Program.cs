02e249b3d3bc679bf83827dccad8da56600f8826
danielSajjadi <info.daniel.sajjadi@gmail.com>
2024-03-17 07:37:02 +0100

    ref instance to delegate

--- two/DelegateBasicExample/DelegateBasicExample/Program.cs
+++ two/DelegateBasicExample/DelegateBasicExample/Program.cs
@@ -8,7 +8,8 @@ namespace DelegateBasicExample
         delegate void LogDel(string text);
         static void Main(string[] args)
         {
-            LogDel logDel = new LogDel(LogTextToFile);
+            Log log = new Log();
+            LogDel logDel = new LogDel(log.LogTextToFile);
             Console.WriteLine("Please enter your name: ");
             var name = Console.ReadLine();
 
@@ -17,12 +18,17 @@ namespace DelegateBasicExample
             
         }
 
-        static void LogTextToScreen(string text)
+    }
+
+    public class Log
+    {
+
+        public void LogTextToScreen(string text)
         {
             Console.WriteLine($"{DateTime.Now} : {text}");
         }
 
-        static void LogTextToFile(string text)
+        public void LogTextToFile(string text)
         {
             using (StreamWriter sw = new StreamWriter(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Log.text"), true))
             {
