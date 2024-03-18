782856ee723f1337b93f364ef6749f125839e053
danielSajjadi <info.daniel.sajjadi@gmail.com>
2024-03-18 10:19:41 +0100

    add new pro. to learn Func

---
+++ four/FuncActionPredicate/FuncActionPredicate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathSum sumCal = new MathSum();
+            Func<int, int, int> cal = sumCal.Sum;
+
+            int calcu = cal(20, 60);
+
+            Console.WriteLine("result -> " + calcu);
+
+        }
+    }
+
+    class MathSum
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        
+    }
+
+
